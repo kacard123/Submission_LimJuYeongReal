40e2994d518a2c0ef273a91d43f45bc4a69a8d16
kacard123 <shalvah623@naver.com>
2022-04-22 01:32:13 +0900

    20220422First

--- Submission LimJuYeong/Assets/01. Scritps/CamFollow.cs
+++ Submission LimJuYeong/Assets/01. Scritps/CamFollow.cs
@@ -4,17 +4,114 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
-    GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        player = GameObject.Find("Player"); // game object player을 찾아서 객체에 지정
+        // 3?? ??? ?
+        Idle, Ready, Tracking
     }
 
-    // Update is called once per frame
-    void Update()
+    // ???? ??
+    private State state
     {
-        Vector3 PlayerPos = player.transform.position; // 플레이어가 조종중인 게임 오브젝트의 위치를 계산
-        transform.position = new Vector3(transform.position.y, PlayerPos.x, transform.position.z); // 플레이어가 조종중인 오브젝트의 y 값만 카메라의 좌표에 넘김
+        set
+        {
+            switch (value)
+            {
+                case State.Idle:
+                    break;
+                case State.Ready:
+                    break;
+                case State.Tracking:
+                    break;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        state = State.Idle;
+    }
+
+    private Transform target;
+    // ??? ??? ? ??? ?????? 
+
+    public float smoothTime = 0.2f;
+    // ??? ??? ? ?? ???? ? ??? ????? ???? ??? ???? ?? ??
+    // ???? ??? ?? ???? ? Vector3 ??? ?? ?? ? ???? ???? ??? ??? ??
+
+
+    private Vector3 lastMovingVelocity;
+    // ????? ??? ??? ??? ???? ?? ???? ?? ??? ???? ???? ?? ??? ????
+    // ?? ???? ?? 
+    private Vector3 targetPosition;
+    // ?? ?? ??? ????? ??? ???? ???? ??? ???? 
+
+    private Camera cam;
+    private float targetZoomSize = 5f;
+
+    private const float roundReadyZoomSize = 14.5f;
+    private const float readyShotZoomSize = 5f;
+    private const float trackingZoomSize = 10f;
+
+    private float lastZoomSpeed;
+
+    private State state
+    {
+        set
+        {
+            switch (value)
+            {
+                case State.Idle:
+                    targetZoomSize = roundReadyZoomSize; //??
+                    break;
+                case State.Ready:
+                    targetZoomSize = readyShotZoomSize; //??
+                    break;
+                case State.Tracking:
+                    targetZoomSize = trackingZoomSize; //??
+                    break;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        cam = GetComponentInChildren<Camera>(); //??
+        state = State.Idle;
+    }
+
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    //???? ?? ???? ??
+    private void Move()
+    {
+        targetPosition = target.transform.position;
+
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition,
+                                            ref lastMovingVelocity, smoothTime);
+
+        transform.position = smoothPosition;
+
+        state = State.Tracking;
+    }
+
+    private void Zoom()
+    {
+        float smoothZoomSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoomSize,
+                                            ref lastZoomSpeed, smoothTime);
+
+        cam.orthographicSize = smoothZoomSize;
+    }
+
+    private void FixedUpdate() // FixedUpdate? ??? ??? ??? ? ?? 
+    {
+        if (target != null)
+        {
+            Move();
+            Zoom();
+        }
     }
 }
