2d116474622504b9c952c717b0b7c02b0ca09a05
kacard123 <shalvah623@naver.com>
2022-05-09 07:15:54 +0900

    20220509_First

--- Submission LimJuYeong/Assets/01. Scritps/ChangeScene.cs
+++ Submission LimJuYeong/Assets/01. Scritps/ChangeScene.cs
@@ -9,5 +9,6 @@ public class ChangeScene : MonoBehaviour
    public void ChangeSceneBtn()
     {
         SceneManager.LoadScene("SampleScene");
+        // 씬매니저를 통해 게임이 플레이되는 "SampleScene"이라는 씬을 불러오기 
     }
 }
